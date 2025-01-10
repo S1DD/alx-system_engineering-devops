bc4272c47fe42dc80091d0289c8f3bb91a5c0c27
Thabiso Molefe <thabisomolefe515@gmail.com>
2025-01-10 15:57:57 -0800

    Tasks done

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates a file called school in /tmp directory
+
+file {'/tmp/school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
